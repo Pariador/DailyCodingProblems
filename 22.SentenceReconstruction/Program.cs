375fbdcc4f977559c640db318217791d45172504
Pariador <pariador.corterian@gmail.com>
2019-03-09 21:44:24 +0200

    Solve 22. Sentence Reconstruction.

--- 22.SentenceReconstruction/Program.cs
+++ 22.SentenceReconstruction/Program.cs
@@ -1,21 +1,93 @@
-﻿using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 static class Program
 {
     static void Main()
     {
+        string str = "thequickbrownfox";
+        string[] words = { "the", "quick", "brown", "fox" };
+        Run(words, str);
+
+        str = "bedbathandbeyond";
+        words = new string[] { "bed", "bath", "bedbath", "and", "beyond", "tha", "ba", "nd" };
+        Run(words, str);
+    }
+
+    static void Run(string[] words, string str)
+    {
+        Console.WriteLine($"Text: {str}");
+        Console.WriteLine($"Words: {string.Join(", ", words)}");
+
+        string[] sentences = Reconstruct(words, str);
+
+        Console.WriteLine("\nSentences:");
+        foreach (var sentence in sentences)
+        {
+            Console.WriteLine(sentence);
+        }
+
+        Console.WriteLine();
     }
 
     static string[] Reconstruct(string[] words, string str)
     {
-        var sentences = new List<StringBuilder>();
+        var complete = new List<string>();
 
-        for (int i = 0; i < str.Length; i++)
+        var partials = new List<string[]>();
+        partials.Add(new string[0]);
+
+        var next = new List<string[]>();
+        while (partials.Any())
         {
+            foreach (var partial in partials)
+            {
+                int partialLength = partial.Sum(word => word.Length);
 
+                foreach (var word in words)
+                {
+                    if (Match(word, str, partialLength))
+                    {
+                        string[] @new = partial.Concat(new string[] { word })
+                            .ToArray();
+
+                        if (partialLength + word.Length == str.Length)
+                        {
+                            complete.Add(string.Join(" ", @new));
+                        }
+                        else
+                        {
+                            next.Add(@new);
+                        }
+                    }
+                }
+            }
+
+            var hold = partials;
+            partials = next;
+            next = hold;
+            next.Clear();
         }
 
-        return null;
+        return complete.ToArray();
+    }
+
+    static bool Match(string word, string str, int i)
+    {
+        if (str.Length - i < word.Length)
+        {
+            return false;
+        }
+
+        for (int w = 0; w < word.Length; w++)
+        {
+            if (word[w] != str[i + w])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
